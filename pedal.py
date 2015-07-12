bcb59fff4edc877a90c9d3f075451b11265b6735
lutskovsky <sergius.lutskovsky@gmail.com>
2015-07-12 20:18:14 +0300

    start

---
+++ pedal.py
@@ -0,0 +1,25 @@
+import sys
+
+if(sys.version_info[0]<3):
+    from Tkinter import *
+else:
+    from tkinter import *
+
+import RPi.GPIO
+
+GPIO.add_event_detect(channel, GPIO.RISING, callback=my_callback, bouncetime=200)
+
+
+root=Tk()
+
+counter=Label(root,text='Pedal was pressed 0 times')
+start_button=Button(root,text='Start')
+stop_button=Button(root,text='Stop')
+
+counter.pack()
+start_button.pack()
+stop_button.pack()
+
+
+root.mainloop()
+
