7d685159b8232fa44e42aec8b0f7eb4d5e044402
lutskovsky <sergius.lutskovsky@gmail.com>
2015-07-12 23:16:18 +0300

    leds

--- pedal.py
+++ pedal.py
@@ -10,9 +10,17 @@ else:
 import RPi.GPIO as GPIO
 
 pedal_pin = 23
+red = 24
+green = 25
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pedal_pin,GPIO.IN)
+GPIO.setup(24,GPIO.OUT)
+GPIO.setup(25,GPIO.OUT)
+
+
+GPIO.output(red, True)
+GPIO.output(green, False)
 
 root = Tk()
 
@@ -35,12 +43,16 @@ def start_count():
     count = 0
     counter_label.set('Pedal was pressed {} times'.format(count))
     GPIO.add_event_detect(pedal_pin, GPIO.RISING, callback=add_count, bouncetime=200)
+    GPIO.output(red, False)
+    GPIO.output(green, True)
 
 def stop_count():
     try:
         GPIO.remove_event_detect(pedal_pin)
     except Exception:
         pass
+    GPIO.output(red, True)
+    GPIO.output(green, False)
 
 start_button=Button(root,text='Start', command=start_count)
 stop_button=Button(root,text='Stop', command=stop_count)
