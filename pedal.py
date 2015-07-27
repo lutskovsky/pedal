9e22566ed3282ad8d8bcbd3a9211c6ed0db94a1e
lutskovsky <lutskovsky@users.noreply.github.com>
2015-07-27 16:29:55 +0300

    nogui

--- pedal.py
+++ pedal.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python
 
-import sys
 import pymysql.cursors
 import RPi.GPIO as GPIO
 import atexit
-
-if sys.version_info[0]<3:
-    from Tkinter import *
-else:
-    from tkinter import *
+import time
 
 @atexit.register
 def cleanup():
@@ -20,6 +15,8 @@ username    = 'root'
 password    = '123456'
 database    = 'pedal'
 this_station_id = 'station1'
+normally_open = True  # set to True if the switch is normally open, to False otherwise
+
 
 db = pymysql.connect (host='localhost', user=username, passwd=password, db=database, autocommit=True)
 cursor = db.cursor()
@@ -36,55 +33,16 @@ GPIO.setup(green, GPIO.OUT)
 GPIO.output(red, True)
 GPIO.output(green, False)
 
-root = Tk()
-
-counter_label = StringVar()
-counter = Label(root, textvariable=counter_label)
-counter.pack()
-
-
-def set_counter(count):
-    if count == 1:
-        t = 'time'
-    else:
-        t = 'times'
-    counter_label.set('Pedal was pressed {} {}'.format(count, t))
-
-count = 0
-set_counter(count)
-
-
 def add_count(channel):
-    global count
-    count += 1
-    set_counter(count)
-    sql = "INSERT INTO pedal_presses (station_id, timestamp) VALUES ('{}', NOW())".format(this_station_id)
-    cursor.execute(sql)
-
-def start_count():
-    try:
-        GPIO.remove_event_detect(pedal)
-    except Exception:
-        pass
-    global count
-    count = 0
-    set_counter(count)
-    GPIO.add_event_detect(pedal, GPIO.RISING, callback=add_count, bouncetime=200)
     GPIO.output(red, False)
     GPIO.output(green, True)
-
-
-def stop_count():
-    try:
-        GPIO.remove_event_detect(pedal)
-    except Exception:
-        pass
+    sql = "INSERT INTO pedal_presses (station_id, timestamp) VALUES ('{}', NOW())".format(this_station_id)
+    cursor.execute(sql)
+    time.sleep(1)
     GPIO.output(red, True)
     GPIO.output(green, False)
 
-start_button=Button(root,text='Start', command=start_count)
-stop_button=Button(root,text='Stop', command=stop_count)
-start_button.pack()
-stop_button.pack()
+GPIO.add_event_detect(pedal, GPIO.RISING if normally_open else GPIO.FALLING, callback=add_count, bouncetime=200)
 
-root.mainloop()
+while True:
+    pass
