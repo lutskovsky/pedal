fd7adab65d86afe2b3e361ed5ae84001f1ca577c
lutskovsky <lutskovsky@users.noreply.github.com>
2015-07-29 09:18:19 +0300

    bounce and leds

--- pedal.py
+++ pedal.py
@@ -16,6 +16,7 @@ password    = '123456'
 database    = 'pedal'
 this_station_id = 'station1'
 normally_open = True  # set to True if the switch is normally open, to False if normally closed
+bouncetime=1000
 
 pedal = 23
 red = 24
@@ -30,19 +31,26 @@ GPIO.setup(pedal, GPIO.IN)
 GPIO.setup(red, GPIO.OUT)
 GPIO.setup(green, GPIO.OUT)
 
-GPIO.output(red, True)
-GPIO.output(green, False)
 
-def add_count(channel):
-    GPIO.output(red, False)
-    GPIO.output(green, True)
-    sql = "INSERT INTO pedal_presses (station_id, timestamp) VALUES ('{}', NOW())".format(this_station_id)
-    cursor.execute(sql)
-    time.sleep(1)
+def red_led():
     GPIO.output(red, True)
     GPIO.output(green, False)
+    
+def green_led():
+    GPIO.output(red, False)
+    GPIO.output(green, True)
 
-GPIO.add_event_detect(pedal, GPIO.RISING if normally_open else GPIO.FALLING, callback=add_count, bouncetime=200)
+def add_record(channel):
+    sql = "INSERT INTO pedal_presses (station_id, timestamp) VALUES ('{}', NOW())".format(this_station_id)
+    cursor.execute(sql)
+
+red_led()
+
+GPIO.add_event_detect(pedal, GPIO.RISING if normally_open else GPIO.FALLING)
+GPIO.add_event_callback(pedal, callback=green_led)
+GPIO.add_event_callback(pedal, callback=add_record, bouncetime=bouncetime)
+
+GPIO.add_event_detect(pedal, GPIO.FALLING if normally_open else GPIO.RISING, callback=red_led)
 
 while True:
     pass
